36dca0cb8a2b8abb2b4d7c7ec7b4f246f8c576a1
Nxtcure-io <manavidubey@gmail.com>
2026-03-20 23:55:26 +0530

    fix: entirely remove custom proxies to finalize pure fallback flow

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/__': {
-        target: 'https://veda-b2b38.firebaseapp.com',
-        changeOrigin: true,
-      }
-    }
-  }
+  plugins: [react()]
 })
